90bc6cdebed297cd256ce6bce13ac4cf10590963
7mochi <flyingcatdm@gmail.com>
2026-05-12 23:46:47 -0500

    chore: aaaaaaaaaaaaaaaa

--- app/main.py
+++ app/main.py
@@ -8,7 +8,6 @@ from scapy.packet import Raw
 HL_INTERNAL_IP = "172.18.0.11"
 HL_PORT = 29428
 AG_PORT = 29420
-
 A2S_INFO_RESPONSE = b"\x49"
 
 
@@ -32,15 +31,15 @@ def modify_packet(pkt):
     modified = False
 
     if packet[UDP].dport == AG_PORT:
-        packet[UDP].dport = HL_PORT
         packet[IP].dst = HL_INTERNAL_IP
+        packet[UDP].dport = HL_PORT
         modified = True
 
         if b"connect" in payload:
             if b"\\_gd\\ag" in payload:
                 payload = payload.replace(b"\\_gd\\ag", b"\\_gd\\valve")
             elif b"\\_gd\\" not in payload:
-                payload += b"\\_gd\\valve"
+                payload = payload.rstrip(b"\x00") + b"\\_gd\\valve\x00"
             packet[Raw].load = payload
 
     elif packet[UDP].sport == HL_PORT:
@@ -48,8 +47,8 @@ def modify_packet(pkt):
         modified = True
 
         if len(payload) > 5 and payload[4:5] == A2S_INFO_RESPONSE:
-            payload = payload.replace(b"Half-Life", b"Adrenaline Gamer")
             payload = payload.replace(b"\x00valve\x00", b"\x00ag\x00")
+            payload = payload.replace(b"Half-Life", b"Adrenaline Gamer")
             packet[Raw].load = payload
 
     if modified:
