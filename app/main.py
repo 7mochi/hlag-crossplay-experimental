213e8d40b17a97da86728276156b06221a99c6bf
7mochi <flyingcatdm@gmail.com>
2026-05-12 23:08:36 -0500

    chore: trying to fix :sob: v3

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import re
+
 from netfilterqueue import NetfilterQueue
 from netfilterqueue import Packet
 from scapy.layers.inet import IP
@@ -9,7 +11,6 @@ from scapy.packet import Raw
 HL_INTERNAL_IP = "172.18.0.11"
 HL_PORT = 29428
 AG_PORT = 29420
-CONNECTIONLESS_HEADER = b"\xff\xff\xff\xff"
 
 
 def modify_packet(pkt: Packet) -> None:
@@ -22,18 +23,19 @@ def modify_packet(pkt: Packet) -> None:
 
         if packet[IP].src == HL_INTERNAL_IP and packet[UDP].sport == HL_PORT:
             packet[UDP].sport = AG_PORT
-            if CONNECTIONLESS_HEADER in payload or b"valve" in payload:
-                payload = payload.replace(b"valve", b"ag")
+            if re.search(b"valve", payload, re.IGNORECASE):
+                payload = re.sub(b"valve", b"ag", payload, flags=re.IGNORECASE)
                 packet[Raw].load = payload
             modified = True
 
         elif packet[UDP].dport == AG_PORT:
             packet[UDP].dport = HL_PORT
             if b"connect" in payload and b"/_gd=valve" not in payload:
-                if b'0 "' in payload:
+                if b"\\" in payload:
+                    parts = payload.split(b"\\", 1)
+                    payload = parts[0] + b"\\/_gd\\valve" + b"\\" + parts[1]
+                elif b'0 "' in payload:
                     payload = payload.replace(b'0 "', b'0 "/_gd=valve')
-                else:
-                    payload = payload.replace(b"connect ", b"connect /_gd=valve ")
                 packet[Raw].load = payload
             modified = True
 
