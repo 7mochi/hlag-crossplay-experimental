6dee9058b694c5d2a44e91247a1d2f352966102a
7mochi <flyingcatdm@gmail.com>
2026-05-13 01:34:07 -0500

    chore: debug v3

--- app/main.py
+++ app/main.py
@@ -12,46 +12,26 @@ def cb(packet):
     raw = packet.get_payload()
     ip = IP(raw)
 
-    if not ip.haslayer(UDP):
-        packet.accept()
-        return
+    if ip.haslayer(UDP):
+        payload = bytes(ip[UDP].payload)
 
-    udp = ip[UDP]
-    payload = bytes(udp.payload)
+        if payload.startswith(b"\xff\xff\xff\xffconnect "):
 
-    direction = "UNKNOWN"
+            idx = payload.find(b"\n")
 
-    if udp.dport == 29420:
-        direction = "CLIENT -> SERVER"
+            if idx != -1:
+                ascii_part = payload[: idx + 1]
+                binary_part = payload[idx + 1 :]
 
-    elif udp.sport == 29428:
-        direction = "SERVER -> CLIENT"
+                print("=== ASCII ===")
+                print(ascii_part)
 
-    print("\n====", direction, "====")
-    print(payload)
-    print(payload.hex())
-
-    try:
-        print(payload.decode("latin1", errors="ignore"))
-    except:
-        pass
+                print("=== BINARY ===")
+                print(binary_part[:32].hex())
 
     packet.accept()
 
 
-nf1 = NetfilterQueue()
-nf1.bind(30, cb)
-
-nf2 = NetfilterQueue()
-nf2.bind(31, cb)
-
-try:
-    import threading
-
-    t = threading.Thread(target=nf1.run)
-    t.start()
-
-    nf2.run()
-
-except KeyboardInterrupt:
-    pass
+nf = NetfilterQueue()
+nf.bind(30, cb)
+nf.run()
